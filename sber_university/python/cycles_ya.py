af199d61de658d8ab1a80c4363dd382750e1a77c
Руслан Каримов <yoursluzhba@gmail.com>
2024-07-02 19:30:51 +0300

    sber university

--- sber_university/python/cycles_ya.py
+++ sber_university/python/cycles_ya.py
@@ -1,27 +1,28 @@
 res = []
-n = 3 * 2
+n = 9 * 2
 for i in range(n):
     if i == 0:
         continue
     line = [' '] * (n - 1)
     c = len(line) // 2
-    start = 0
-    end = len(line) - 1
+    # start = 0
+    # end = len(line) - 1
     k = 1
     if i == 1 or i == len(line):
         start = c
-        end = start + 1
+        end = start
     elif c + 1 < i:
         start += 1
         end -= 1
     else:
-        start = c - (i - 1)
-        end = c + (i - 1)
-    for j in range(start, end):
-        if j < c:
-            k = k + 1
-        elif j > c:
-            k = k - 1
+        start -= 1
+        end += 1
+    for j in range(start, end + 1):
         line[j] = str(k)
+        if j >= c:
+            k = k - 1
+        else:
+            k = k + 1
+
     res.append(''.join(line))
 print('\n'.join(res))
