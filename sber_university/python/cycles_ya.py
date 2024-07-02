643136cddbb82034c3f30d334be93c264201f4b6
Karimov Ruslan <Karimov.R.M@sbertech.ru>
2024-07-02 17:43:27 +0300

    sber university

--- sber_university/python/cycles_ya.py
+++ sber_university/python/cycles_ya.py
@@ -5,15 +5,23 @@ for i in range(n):
         continue
     line = [' '] * (n - 1)
     c = len(line) // 2
-    if i == 1:
-        line[c] = str(i)
-        continue
-
-    if c > i:
+    start = 0
+    end = len(line) - 1
+    k = 1
+    if i == 1 or i == len(line):
+        start = c
+        end = start + 1
+    elif c + 1 < i:
+        start += 1
+        end -= 1
+    else:
         start = c - (i - 1)
         end = c + (i - 1)
-    else:
-        print('')
-
+    for j in range(start, end):
+        if j < c:
+            k = k + 1
+        elif j > c:
+            k = k - 1
+        line[j] = str(k)
     res.append(''.join(line))
 print('\n'.join(res))
